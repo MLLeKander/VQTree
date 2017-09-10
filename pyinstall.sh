d88c0fda05eccafb5158904078ca24a1904b6525
Michael LeKander <m.l.lekander@gmail.com>
2017-09-10 23:26:50 +0200

    Don't reset with pyinstall.

--- pyinstall.sh
+++ pyinstall.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-reset && sudo rm -rf /usr/local/lib/python2.7/dist-packages/*{_{mean,k},vq}tree* build && sudo python setup.py install
+sudo rm -rf /usr/local/lib/python2.7/dist-packages/*{_{mean,k},vq}tree* build && sudo python setup.py install
